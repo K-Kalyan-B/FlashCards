fc8a63c38ff086b624f107e241b39692acda2c30
K-Kalyan-B <khlvn2005@gmail.com>
2025-05-02 20:45:16 +0530

    flashcard shows answer when flipped

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,10 +1,10 @@
 import flashcards from '../data/flashcards.json';
 
-const FlashCard = ({currentIndex}) => {
+const FlashCard = ({currentIndex,Flipped}) => {
    return (
     <div>
         <div className="flashcard">
-            <h3>{flashcards[currentIndex].answer}</h3>
+            {Flipped ? <h2>{flashcards[currentIndex].question}</h2> : <h2>{flashcards[currentIndex].answer}</h2>}
         </div>
     </div>
    );
--- src/components/FlashCardContainer.jsx
+++ src/components/FlashCardContainer.jsx
@@ -5,13 +5,20 @@ import FlashCard from "./FlashCard.jsx";
 const FlashCardContainer = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [Flipped, setFlipped] = useState(false);
+
+    const handleFlipped = ()=>{
+        setFlipped(!Flipped);
+    }
     return (
         <div>
             <h1>Flash Card Container</h1>
             <p>This is the flash card container component.</p>
             <div className="flash-card-container">
                 <Progress progress={currentIndex} total={flashcards.length}/>
-                <FlashCard currentIndex = {currentIndex}/>
+                <FlashCard currentIndex = {currentIndex} Flipped = {Flipped}/>
+                <div className="flash-card-buttons">
+                    <button onClick={handleFlipped}>{Flipped ? `Show Answer` : `Hide Answer`}</button>
+                </div>
             </div>
         </div>
     )
