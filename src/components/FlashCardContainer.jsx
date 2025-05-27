49e154ae3d972e287015d24196c0b3e8d3a2189f
K-Kalyan-B <khlvn2005@gmail.com>
2025-05-27 19:11:52 +0530

    changed index.html

--- src/components/FlashCardContainer.jsx
+++ src/components/FlashCardContainer.jsx
@@ -27,9 +27,10 @@ const FlashCardContainer = () => {
         <div>
             <h1>Flash Card Container</h1>
             <p>This is the flash card container component.</p>
+            {/*create a subject menu dropdown to select the subject of flashcards*/}
             <div className="flash-card-container">
                 <Progress progress={currentIndex+1} total={flashcards.length}/>
-                <FlashCard currentIndex = {currentIndex} Flipped = {Flipped}/>
+                <FlashCard currentIndex = {currentIndex} Flipped = {Flipped}/>{/*pass Q&A instead of index */}
                 <div className="flash-card-buttons">
                     <button className = "buttons" onClick={handlePrevious}>&lt;&lt; Previous</button>
                     <button className = "buttons" onClick={handleFlipped}>{Flipped ? `Hide Answer` : `Show Answer`}</button>
